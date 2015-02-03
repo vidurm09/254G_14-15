dc57787dc217bab0ad1aeba60c4a60df36677a43
Trevor Salom <trvrsalom@gmail.com>
2015-02-02 17:40:17 -0800

    motor setup for new bot

--- robotCode/robotMain.c
+++ robotCode/robotMain.c
@@ -1,3 +1,22 @@
+#pragma config(Sensor, in1,    autoPot,        sensorPotentiometer)
+#pragma config(Sensor, in2,    posPot,         sensorPotentiometer)
+#pragma config(Sensor, dgtl1,  rDriveEncoder,  sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  rLiftEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  lDriveEncoder,  sensorQuadEncoder)
+#pragma config(Sensor, dgtl7,  lLiftEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl9,  liftDetectLeft, sensorTouch)
+#pragma config(Sensor, dgtl10, liftDetectRight, sensorTouch)
+#pragma config(Motor,  port1,           driveLB,       tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port2,           driveLF,       tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port3,           liftLB,        tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           liftLT,        tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port5,           intakeR,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port6,           intakeL,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port7,           liftRT,        tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port8,           liftRB,        tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port9,           driveRF,       tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port10,          driveRB,       tmotorVex393_HBridge, openLoop, reversed)
+
 #pragma platform(VEX)
 
 //Competition Control and Duration Settings
