c33e98f448960e4480370cb12eb2042a3b7acd33
Trevor Salom <trvrsalom@gmail.com>
2015-02-02 17:51:52 -0800

    added drive awards

--- robotCode/robotDrive.c
+++ robotCode/robotDrive.c
@@ -1,1 +1,33 @@
+float ignoreDriveError = 40;
+float driveKp = 1.5;
+float driveKi = 0.0;
+float driveKd = 0.0;
+bool toDriveStream = false;
+float rPrevError = 0;
+float rIntegral = 0;
+float lPrevError = 0;
+float lIntegral = 0;
+float rError;
+float lError;
+float lDerivative;
+float rDerivative;
+float driveSetLPt = 0;
+float driveSetRPt = 0;
 
+task drivePID() {
+	while(true) {
+		rError = driveSetRPt + SensorValue[rDriveEncoder];
+		lError = driveSetLPt - SensorValue[lDriveEncoder];
+		lIntegral += lError;
+		rIntegral += rError;
+		rDerivative = rError - rPrevError;
+		lDerivative = lError - lPrevError;
+		motor[driveRB] = (driveKp*rError) + (driveKi*rIntegral) + (driveKd*rDerivative);
+		motor[driveRF] = (driveKp*rError) + (driveKi*rIntegral) + (driveKd*rDerivative);
+		motor[driveLB] = (driveKp*lError) + (driveKi*lIntegral) + (driveKd*lDerivative);
+		motor[driveLF] = (driveKp*lError) + (driveKi*lIntegral) + (driveKd*lDerivative);
+		if(toDriveStream) {
+			writeDebugStreamLine("%f, %f,", rError, lError);
+		}
+	}
+}
